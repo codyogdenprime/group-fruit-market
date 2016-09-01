de1981151452790459de1711cd693bf5883f15d4
Cody Ogden <cody@codyogden.com>
2016-09-01 13:52:15 -0500

    Establish Wallet

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var wallet = function () {
+var walletFunc = function () {
 	var total = 0;
 	this.add = function ( num ) {
 		total = Number( total + num );
@@ -11,7 +11,10 @@ var wallet = function () {
 	};
 };
 
+var wallet = new walletFunc();
 var inventory = []; // User's Inventory ( of objects )
+var programStatus = true;
+
 
 var market = [
 {
@@ -20,3 +23,20 @@ var market = [
 	photo: "http://placehold.it/100"
 }
 ];
+
+// Five minute timer function
+// Run this function when the program starts
+var fiveMinuteTimer = function () {
+	// Shut Down and Sell Off after 5 minutes
+	setTimeout( shutdownFunction, 300000);
+};
+
+// Fifteen seconds timer function
+// Run this function every fifteen seconds
+var fifteenSecondsTimer = function () {
+	// If the program is running is true
+	if( programStatus === true ) {
+		// Set a time out to run Change Price again
+		setTimeout( changePrice(), 15000 );
+	}
+};
