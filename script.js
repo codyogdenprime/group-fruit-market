eb66c4c304f9dc574d8bb62504039dbbb7912def
Cody Ogden <cody@codyogden.com>
2016-09-01 13:40:14 -0500

    Create programStatus var. Add timer functions

---
+++ script.js
@@ -0,0 +1,40 @@
+var wallet = function () {
+	var total = 0;
+	this.add = function ( num ) {
+		total = Number( total + num );
+	};
+	this.sub = function ( num ) {
+		total = Number ( total - num );
+	};
+	this.total = function () {
+		return total;
+	};
+};
+
+var inventory = []; // User's Inventory ( of objects )
+var programStatus = true;
+
+var market = [
+{
+	pie: "Apple",
+	price: 1,
+	photo: "http://placehold.it/100"
+}
+];
+
+// Five minute timer function
+// Run this function when the program starts
+var fiveMinuteTimer = function () {
+	// Shut Down and Sell Off after 5 minutes
+	setTimeout( shutdownFunction, 300000);
+};
+
+// Fifteen seconds timer function
+// Run this function every fifteen seconds
+var fifteenSecondsTimer = function () {
+	// If the program is running is true
+	if( programStatus === true ) {
+		// Set a time out to run Change Price again
+		setTimeout( changePrice(), 15000 );
+	}
+};
