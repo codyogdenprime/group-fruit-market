54033db2294afceefe14e08f4733ef88b5f7d68e
Will Cruzen <will.cruzen@gmail.com>
2016-09-01 13:55:24 -0500

    pseudo buy/sell written

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ var market = [
 	photo: "http://placehold.it/100"
 }
 ];
+var i = 0;
+var buyPi = function() {
+  console.log('in buyPi');
+  //establish pie type
+  var piType = market[i].pie;
+  console.log('Pie type:', piType);
+  // //subtract $$$ from wallet
+  total = wallet.sub(market[i].price);
+  console.log(total);
+  // //add purchased pie to Inventory
+  // inventory.push(market[i]);
+  //update average purchased price
+
+  //display on DOM
+};
+
+console.log(buyPi());
+
+var sellPi = function() {
+  //establish pie type
+
+  //add $$$ to wallet
+
+  //remove purchased pie from Inventory
+
+  //update average purchased price
+
+  //display on DOM
+};
