e15e3c8a070c08d6977163c105d34408f9a00ec0
Ben Mahowald <ben.mahowald@gmail.com>
2016-09-01 15:10:44 -0500

    add calculate price function

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ var wallet = function () {
 
 var inventory = []; // User's Inventory ( of objects )
 
+var running = function () {
+	//timmer that counts down if 5 minutes has elapsed
+};
+
 var market = [
 {
 	pie: "Apple",
@@ -20,3 +24,41 @@ var market = [
 	photo: "http://placehold.it/100"
 }
 ];
+
+var start = function () {
+		//generate random price for each pie price
+		var calculatePrice = function () {
+	  return Number( ( (Math.random() * 9.99) + 0.5 ).toFixed(2) );
+	}
+
+	var randomNum = function () {
+	  var price = calculatePrice();
+	  if ( price > 9.99 || price < 0.5 || price === undefined ) {
+	    randomNum();
+	  } else {
+	    return price;
+	  }
+	};
+		for (var i = 0; i < market.length; i++) {
+			market[i].price = price;ben
+		}
+}
+
+console.log( 'randomNum function price output:', randomNum() );
+
+			market[i].price = cost;
+		}
+	// calculatePrice();
+  // displayPies();
+};
+
+
+var sellOff = function () {
+	if (running === true) {
+		//update market close prices
+	} else {
+		//alert shopper the market is closed
+		//empty pies from bag
+		//compare money spent on pies to ending market price on pies
+	}
+};
